79ccc78fe0a7848621094e5f36473492f4c5075d
Moe <alshaibm@uwindsor.ca>
2022-11-20 16:43:35 -0500

    added boilerplate

---
+++ hostel-service/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main class="bg">
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+.bg {
+  background-color: gray;
+}
+</style>
