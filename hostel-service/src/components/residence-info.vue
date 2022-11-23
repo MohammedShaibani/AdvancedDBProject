46c7eeaf227ef0146c63e5f42e3e3dac3e471f66
Dariq A <72431760+DecodedSpark@users.noreply.github.com>
2022-11-22 22:58:37 -0500

    Update residence-info.vue

--- hostel-service/src/components/residence-info.vue
+++ hostel-service/src/components/residence-info.vue
@@ -1,26 +1,60 @@
 <template>
     <div class="form-container mx-auto my-5">
+        <div class="row-container mx-auto">
+            <div class="column-container mx-auto mr-2">
+                <label class="my-auto">Hostel Building Name</label>
+                <input class="input-style my-auto" type="text">
+            </div>
+
+            <div class="column-container mx-auto ml-2">
+                <label class="my-auto">Telephone #</label>
+                <input class="input-style my-auto" type="text">
+            </div>
+        </div>
+
         <div class="column-container mx-auto">
-            <label class="my-auto">Hostel Building Name</label>
-            <input class="input-style my-auto" type="text">
-            <label class="my-auto">Telephone #</label>
-            <input class="input-style my-auto" type="text">
             <label class="my-auto">Address</label>
             <input class="input-style my-auto" type="text">
-            <v-btn class="my-auto mt-5 btn-style" rounded outlined >Back</v-btn>
         </div>
+
+        <div class="title-container mx-auto">
+            <p>Lease Information</p>
+        </div>
+
         <div class="column-container mx-auto">
-            <label class="my-auto"># Terms</label>
-            <input class="input-style my-auto" type="text">
-            <label class="my-auto">Room #</label>
-            <input class="input-style my-auto" type="text">
-            <label class="my-auto">Start Date</label>
-            <input class="input-style my-auto py-6" type="text">
-            <label class="my-auto">End Date</label>
-            <input class="input-style my-auto py-10" type="text">
-            <div class="btn-container mt-5">
-                <v-btn class="my-auto btn-style mr-1" rounded outlined >Save</v-btn>
-                <v-btn class="my-auto btn-style ml-1" rounded outlined >Update</v-btn>
+            <div class="row-container mx-auto">
+                <div class="column-container mx-auto mr-2">
+                    <label class="my-auto"># Terms</label>
+                    <input class="input-style my-auto" type="text">
+                </div>
+
+                <div class="column-container mx-auto ml-2">
+                    <label class="my-auto">Room #</label>
+                    <input class="input-style my-auto" type="text">
+                </div>
+            </div>
+
+            <div class="row-container mx-auto">
+                <div class="column-container mx-auto mr-2">
+                    <label class="my-auto">Start Date</label>
+                    <input class="input-style my-auto" type="text">
+                </div>
+
+                <div class="column-container mx-auto ml-2">
+                    <label class="my-auto">End Date</label>
+                    <input class="input-style my-auto" type="text">
+                </div>
+            </div>
+
+            <div class="row-container mx-auto">
+                <div class="column-container mx-auto mr-2">
+                    <v-btn class="my-auto btn-style mr-1" rounded outlined >Request New Lease</v-btn>
+                </div>
+
+                <div class="column-container mx-auto ml-2">
+                    <v-btn class="my-auto btn-style mr-1" rounded outlined >Cancel Lease</v-btn>
+                    <v-btn class="my-auto btn-style ml-1" rounded outlined >Invoices</v-btn>
+                </div>
             </div>
         </div>
     </div>
@@ -36,7 +70,7 @@ export default {
 <style scoped>
 .form-container {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     background-color: #06A77D;
     color: #EEF0F2;
     justify-content: center;
@@ -51,7 +85,21 @@ export default {
     flex-direction: column;
     justify-content: center;
     align-content: flex-start;
+    padding: 10px;
+}
 
+.row-container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-content: flex-start;
+    padding: 10px;
+    margin: 10px;
+}
+
+.title-container {
+    text-decoration: underline;
+    margin: 50px;
 }
 
 .input-style {
